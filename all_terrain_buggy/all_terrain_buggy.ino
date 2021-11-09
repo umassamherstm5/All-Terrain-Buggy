a2a1597ad9c62ab242481ef14ae1febcba48e0f0
Akshat412 <akshat412000@gmail.com>
2021-11-09 12:20:02 -0500

    Fixed getState conditionals

--- all_terrain_buggy/all_terrain_buggy.ino
+++ all_terrain_buggy/all_terrain_buggy.ino
@@ -8,6 +8,8 @@
 #define CHANNEL_1 5 // channel 1 of the controller
 #define CHANNEL_2 6 // channel 2 of the controller 
 
+#define DEBUG 0
+
 int sig1 = 0;
 int sig2 = 0; 
 
@@ -21,43 +23,41 @@ void setup() {
 }
 
 void loop() {
-  sig1 = pulseIn(CHANNEL_1, HIGH, 10000); // read pulse on channel 1 
-  sig2 = pulseIn(CHANNEL_2, HIGH, 10000); // read pulse on channel 2
-
-  Serial.print(sig1);
-  Serial.print(" ");
-  Serial.println(sig2); 
+  sig1 = pulseIn(CHANNEL_1, HIGH);
+  sig2 = pulseIn(CHANNEL_2, HIGH);
 
   // states: OFF = 0, FORWARD = 1, BACKWARD = 2, LEFT = 3, RIGHT = 4 
-  delay(10); 
-}
-
-int getState(int sig1, int sig2) {
-  // possible values are 1, 0 and -1 
-  if((1520 < sig1 < 1540) && (1520 < sig2 < 1540)) {
+  if(((sig1 < 1560) && (sig1 > 1500)) && ((sig2 < 1560) && (sig2 > 1500))) {
     // both signals are 0, state: OFF
     Serial.println("OFF"); 
+    
   }
 
-  else if((sig1 > 1540) && (1520 < sig2 < 1540)) {
+  else if((sig1 > 1560) && ((sig2 < 1560) && (sig2 > 1500))) {
     // sig1 is 1, sig2 is 0, state: FORWARD
     Serial.println("FORWARD"); 
+    
   }
 
-  else if((sig1 < 1520) && (1520 < sig2 < 1540)) {
+  else if((sig1 < 1500) && ((sig2 < 1560) && (sig2 > 1500))) {
     // sig1 is -1, sig2 is 0, state: BACKWARD
     Serial.println("BACKWARD"); 
+    
   }
 
-  else if(sig2 > 1540) {
+  else if(sig2 > 1500) {
     // sig1 is X, sig2 is 1, state: RIGHT
     Serial.println("RIGHT"); 
+    
   }
 
-  else if(sig2 < 1520) {
+  else if(sig2 < 1560) {
     // sig1 is X, sig2 is -1, state: LEFT
     Serial.println("LEFT"); 
+    
   }
 
-  return 0;
+  else if(sig1 == 0 || sig2 == 0) {
+    Serial.println("ERROR"); 
+  }
 }
